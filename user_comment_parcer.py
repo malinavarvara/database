bf887e39959a25e1503631a58933930a1a91e46d
malinavarvara <malina.varvara@mail.ru>
2023-10-23 15:38:29 +0300

    create

--- user_comment_parcer.py
+++ user_comment_parcer.py
@@ -9,15 +9,19 @@ from selenium.webdriver.support.ui import WebDriverWait
 from ordered_set import OrderedSet
 from bs4 import BeautifulSoup
 
+
 def highlight(driver, element):
     """Highlights (blinks) a Selenium Webdriver element"""
+
     def apply_style(s):
         driver.execute_script("arguments[0].setAttribute('style', arguments[1]);",
                               element, s)
+
     original_style = element.get_attribute('style')
     apply_style("background: yellow; border: 2px solid red;")
     time.sleep(.3)
     apply_style(original_style)
+
 
 def is_element_exist_by_id(driver, id):
     try:
@@ -25,11 +29,13 @@ def is_element_exist_by_id(driver, id):
     except TimeoutException:
         return False
 
-def is_element_exist_by(driver,by, id):
+
+def is_element_exist_by(driver, by, id):
     try:
         WebDriverWait(driver, 5).until(EC.presence_of_element_located((by, id)))
     except TimeoutException:
         return False
+
 
 def post_comment_parser_test(url):
     # Создание дравера с опциями (чтобы не спамил ошибками)
@@ -40,48 +46,50 @@ def post_comment_parser_test(url):
     # Открываем веб-сайт
     driver.get(url)
     time.sleep(5)
-    
+
     elements_classes = driver.find_elements(By.CSS_SELECTOR, "[class]")
-    
+
     comm_pattern = r"comment__commentContent-.*"
-    elements_comms = OrderedSet([element for element in elements_classes if re.search(comm_pattern, element.get_attribute("class"))])
-    
+    elements_comms = OrderedSet(
+        [element for element in elements_classes if re.search(comm_pattern, element.get_attribute("class"))])
+
     Bad_counter = 0
     while True:
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            
+
         new_elements_classes = driver.find_elements(By.CSS_SELECTOR, "[class]")
         try:
-            new_elements_comms = OrderedSet([element for element in new_elements_classes if re.search(comm_pattern, element.get_attribute("class"))])
+            new_elements_comms = OrderedSet([element for element in new_elements_classes if
+                                             re.search(comm_pattern, element.get_attribute("class"))])
             new_elements_comms |= elements_comms
-            #more_comment_str = "коммент"
+            # more_comment_str = "коммент"
             if len(elements_comms) == len(new_elements_comms):
-                Bad_counter+=1
-                #button = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH,'//div[@class="more-comments-button__block-3z more-comments-button__text-1C"]')))
-                #print(button.text)
-                #driver.execute_script("arguments[0].style.transform='scale(1)';", driver.find_element(By.TAG_NAME,"body"))
-                #highlight(driver, button)
-                #driver.execute_script("arguments[0].click();", button)
-            #else:
+                Bad_counter += 1
+                # button = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH,'//div[@class="more-comments-button__block-3z more-comments-button__text-1C"]')))
+                # print(button.text)
+                # driver.execute_script("arguments[0].style.transform='scale(1)';", driver.find_element(By.TAG_NAME,"body"))
+                # highlight(driver, button)
+                # driver.execute_script("arguments[0].click();", button)
+            # else:
             #    Bad_counter = 0
             elements_comms |= new_elements_comms
         except:
-            Bad_counter+=1
-        if(Bad_counter>=3):
+            Bad_counter += 1
+        if (Bad_counter >= 3):
             break
-     
+
     for el in elements_comms:
-        el_soup = BeautifulSoup(el.get_attribute('innerHTML'),'html.parser')
+        el_soup = BeautifulSoup(el.get_attribute('innerHTML'), 'html.parser')
         link_pattern = re.compile(r"Link Link_theme_normal Link_view_default comment-header__nameLink.*")
-        links = el_soup.find_all(class_= link_pattern)
-        if links is not None:  
+        links = el_soup.find_all(class_=link_pattern)
+        if links is not None:
             for link in links:
-                user_url_part = link.get("href") 
-                
+                user_url_part = link.get("href")
+
                 print(user_url_part)
     driver.quit()
+
 
 if __name__ == '__main__':
     test_url = 'https://dzen.ru/b/ZTP3gQJ23RI2zQz-?referrer_clid=1400&'
     post_comment_parser_test(test_url)
-    
--- user_post_parcer.py
+++ user_post_parcer.py
@@ -7,20 +7,22 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from sql_table import insert_post
 
-def is_element_exist_by_id(driver,id):
+
+def is_element_exist_by_id(driver, id):
     try:
         WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, id)))
     except TimeoutException:
         return False
 
+
 def number_to_changesz(name):
     name = name[:-7]
     index = 0
-    likes=0
+    likes = 0
     if name[len(name) - 1].isdigit() or name[len(name) - 2].isdigit():
         for i in reversed(range(0, len(name))):
             if name[i].isdigit() and not (name[i - 1].isdigit()) and not (name[i - 2].isdigit()):
-                index = i 
+                index = i
                 break
         name = name[index:]
         name = name.replace(',', '.')
@@ -45,44 +47,44 @@ def user_post_parser_zher(url):
     current_id = 0
     current_id_txt = 'zen-row-' + str(current_id)
 
-    while current_id!=6:
+    while current_id != 6:
         try:
             # Поиск элемента с текущим id
             element = driver.find_element(By.ID, current_id_txt)
-            #ссылка
+            # ссылка
             text = element.get_attribute('innerHTML')
             url_pattern = r'https://[\S]+'
             urls = re.findall(url_pattern, text)
-            if len(urls)!=0:
-                likes=0
-                comments=0
-                #likes
+            if len(urls) != 0:
+                likes = 0
+                comments = 0
+                # likes
                 name = element.find_element(By.CLASS_NAME, "zen-ui-button-like__content").get_attribute('innerHTML')
-                likes=number_to_changesz(name)
+                likes = number_to_changesz(name)
                 # comments
                 name = element.find_element(By.CLASS_NAME, "zen-ui-button-footer__content").get_attribute('innerHTML')
                 comments = number_to_changesz(name)
-                if comments>0:
-                    comments_url=element.find_element(By.CLASS_NAME, "card-layer-channel-footer-view__social-meta").get_attribute('innerHTML')
+                if comments > 0:
+                    comments_url = element.find_element(By.CLASS_NAME,
+                                                        "card-layer-channel-footer-view__social-meta").get_attribute(
+                        'innerHTML')
                     url_pattern_com = r'https://[\S]+'
                     urls_com = re.findall(url_pattern_com, comments_url)
                     print(urls_com)
 
-                urls_post=urls[len(urls) - 1]
-                insert_post(url,int(likes),int(comments),urls_post[:-1])
-
-
+                urls_post = urls[len(urls) - 1]
+                insert_post(url, int(likes), int(comments), urls_post[:-1])
 
             # Перейдите к следующему id
             current_id = current_id + 1
             current_id_txt = 'zen-row-' + str(current_id)
 
             scroll_count = 0
-            while is_element_exist_by_id(driver,current_id_txt)==False and scroll_count<3:
-                scroll_count+=1
+            while is_element_exist_by_id(driver, current_id_txt) == False and scroll_count < 3:
+                scroll_count += 1
                 driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-                
-            if is_element_exist_by_id(driver,current_id_txt)==False:
+
+            if is_element_exist_by_id(driver, current_id_txt) == False:
                 print("Конец страницы")
                 break
         except Exception as e:
@@ -91,7 +93,8 @@ def user_post_parser_zher(url):
             print(type(e), e)
             break
     driver.quit()
-    
+
+
 if __name__ == '__main__':
     test_url = 'https://dzen.ru/id/652a7df17a8a8e15f847988f'
     user_post_parser_zher(test_url)
